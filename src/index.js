27dfed0318d2bca3c3c714bb35ceb1cad503ab24
sujith <sujithsuresh246@gmail.com>
2020-04-09 20:12:39 +0530

    webpack config added

---
+++ src/index.js
@@ -0,0 +1,3 @@
+import num from './test'
+const x = 24;
+console.log(`Imported value ${num} from test.js file!!!!!!!! const x is ${x}`);
---
+++ src/test.js
@@ -0,0 +1,2 @@
+console.log('module imported!')
+export default 888;
