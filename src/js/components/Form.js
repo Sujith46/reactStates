95fddd05f8e9ab32d5e587c7acef65aa949c3356
sujith <sujithsuresh246@gmail.com>
2020-04-10 22:46:28 +0530

    useState hook working

--- src/js/components/Form.js
+++ src/js/components/Form.js
@@ -3,14 +3,39 @@ import ReactDOM from "react-dom";
 
 const App = (props) => {
 
-  const [count, setCount] = useState(props.count)
+  const [notes, setNotes] = useState([])
+  const [title, setTitle] = useState('')
 
+  const addNote = (e) => {
+    e.preventDefault()
+    setNotes([
+      ...notes,
+      {
+        title
+      }
+    ])
+    setTitle('')
+  }
+
+
+  const removeNote = (title) => {
+    setNotes(notes.filter((note) => note.title !== title))
+  }
 
   return (
     <div>
-      <p>The current count is {count}</p>
-      <buttom onClick={() => setCount(count - 1)}>-1</buttom>
-      <buttom onClick={() => setCount(count + 1)}>+1</buttom>
+      <h1>Notes</h1>
+      {notes.map((note) => (
+        <div key={note.title}>
+          <h3>{note.title}</h3>
+          <button onClick ={() => removeNote(note.title)}>x</button>
+        </div>
+      ))}
+      <p>Add notes</p>
+     <form onSubmit={addNote}>
+       <input value={title} onChange={(e) => setTitle(e.target.value)}></input>
+       <button>add note</button>
+     </form>
     </div>
   )
 }
